6b313bd5525bd74a803ddf89e7ec167c00ff9549
Jack <jackfagner@users.noreply.github.com>
2017-08-31 10:00:08 +0200

    Update tiny-framework.js

--- tiny-framework.js
+++ tiny-framework.js
@@ -7,14 +7,21 @@ Window.prototype.on = function (type, listener) { return this.addEventListener(t
 Window.prototype.off = function (type, listener) { return this.removeEventListener(type, listener, false); };
 (typeof EventTarget === "undefined") ? (Document.prototype.off = Element.prototype.off = Window.prototype.off) : (EventTarget.prototype.off = Window.prototype.off);
 
-Element.prototype.attr = function (name, value) { return (typeof value === "undefined" ? this.getAttribute(name) : this.setAttribute(name, value)); };
-Element.prototype.html = function (value) { return (typeof value === "undefined" ? this.innerHTML : this.innerHTML = value); };
-Element.prototype.append = function (value) { this.innerHTML = this.innerHTML += value; };
+//Element.prototype.attr = function (name, value) { return (typeof value === "undefined" ? this.getAttribute(name) : this.setAttribute(name, value)); };
+//Element.prototype.html = function (value) { return (typeof value === "undefined" ? this.innerHTML : this.innerHTML = value); };
+//Element.prototype.append = function (value) { this.innerHTML = this.innerHTML += value; };
 //Element.prototype.closest
+//Element.prototype.val = function (value) { return (typeof value === "undefined" ? this.value : this.value = value); };
 Element.prototype.is = Element.prototype.matches || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector;
+Array.prototype.each = Array.prototype.forEach;
 Array.prototype.first = function () { return (this.length === 0 ? null : this[0]); };
 Array.prototype.last = function () { return (this.length === 0 ? null : this[this.length - 1]); };
-Element.prototype.val = function (value) { return (typeof value === "undefined" ? this.value : this.value = value); };
+
+Element.prototype.attr = function (name, value) { return [ this ].attr(name, value); };
+Element.prototype.val = function (value) { return [ this ].val(value); };
+Element.prototype.append = function (html) { return [ this ].append(html); };
+Element.prototype.html = function (value) { return [ this ].html(value); };
+Element.prototype.text = function (value) { return [ this ].text(value); };
 
 String.prototype.htmlEncode = function () {
     var map = { "&": "&amp;", "\"": "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;" };
@@ -29,13 +36,40 @@ String.prototype.htmlDecode = function () {
 Array.prototype.attr = function (name, value) { 
     if (typeof value === "undefined")
         return this.length > 0 ? this[0].getAttribute(name) : null;
-    this.forEach(function (e,i) { e.attr(name, value); });
+    this.forEach(function (e,i) { e.setAttribute(name, value); });
     return this;
 };
-//Array.prototype.val = function (value) { if (this.length > 0) { this[0].val(value); }; return this; };
-//Array.prototype.html = function (value) { if (this.length > 0) { this[0].html(value); }; return this; };
-//Array.prototype.append = function (value) { if (this.length > 0) { this[0].append(value); }; return this; };
-Array.prototype.on = function (type, listener) { if (this.length > 0) { this[0].on(type, listener); }; return this; };
-Array.prototype.off = function (type, listener) { if (this.length > 0) { this[0].off(type, listener); }; return this; };
+Array.prototype.val = function (value) { 
+    if (typeof value === "undefined")
+        return this.length > 0 ? this[0].value : null;
+    this.forEach(function (e,i) { e.value = value; });
+    return this;
+};
+Array.prototype.append = function (html) {
+    this.forEach(function (e,i) { e.innerHTML += html; });
+    return this;
+};
+Array.prototype.html = function (value) { 
+    if (typeof value === "undefined")
+        return this.length > 0 ? this[0].innerHTML : null;
+    this.forEach(function (e,i) { e.innerHTML = value; });
+    return this;
+};
+Array.prototype.text = function (value) { 
+    if (typeof value === "undefined")
+        return this.length > 0 ? this[0].innerText : null;
+    this.forEach(function (e,i) { e.innerText = value; });
+    return this;
+};
+Array.prototype.on = function (type, listener) {
+    this.forEach(function (e,i) { e.on(type, listener); });
+    return this;
+};
+Array.prototype.off = function (type, listener) {
+    this.forEach(function (e,i) { e.off(type, listener); });
+    return this;
+};
+//Array.prototype.on = function (type, listener) { if (this.length > 0) { this[0].on(type, listener); }; return this; };
+//Array.prototype.off = function (type, listener) { if (this.length > 0) { this[0].off(type, listener); }; return this; };
 Array.prototype.is = function (selector) { if (this.length > 0) { return this[0].is(selector); }; return false; };
 Array.prototype.closest = function (selector) { if (this.length > 0) { return this[0].closest(selector); }; return null; };
