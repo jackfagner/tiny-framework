f1d6ac425bedcca9e68750a9815324df128cccd5
Jack <jackfagner@users.noreply.github.com>
2017-08-31 09:23:25 +0200

    Update tiny-framework.js

--- tiny-framework.js
+++ tiny-framework.js
@@ -3,17 +3,19 @@
 var $ = function (id) { return Array.prototype.slice.call(document.querySelectorAll(id)); };
 
 Window.prototype.on = function (type, listener) { return this.addEventListener(type, listener, false); };
-if (typeof EventTarget == "undefined") //Chromium
+if (typeof EventTarget === "undefined") //Chromium
     Document.prototype.on = Element.prototype.on = Window.prototype.on;
 else
     EventTarget.prototype.on = Window.prototype.on;
     
-Element.prototype.attr = function (name, value) { return (typeof value == "undefined" ? this.getAttribute(name) : this.setAttribute(name, value)); };
-Element.prototype.html = function (value) { return (typeof value == "undefined" ? this.innerHTML : this.innerHTML = value); };
+Element.prototype.attr = function (name, value) { return (typeof value === "undefined" ? this.getAttribute(name) : this.setAttribute(name, value)); };
+Element.prototype.html = function (value) { return (typeof value === "undefined" ? this.innerHTML : this.innerHTML = value); };
 Element.prototype.append = function (value) { this.innerHTML = this.innerHTML += value; };
-Element.prototype.getParentByTagName = function (tagName) { return (this.parentNode["getParentByTagName"] ? (this.parentNode.nodeName.toLowerCase() == tagName.toLowerCase() ? this.parentNode : this.parentNode.getParentByTagName(tagName)) : null); };
-Array.prototype.first = function () { return (this.length == 0 ? null : this[0]); };
-Array.prototype.last = function () { return (this.length == 0 ? null : this[this.length - 1]); };
+//Element.prototype.closest
+Element.prototype.is = Element.prototype.matches || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector;
+Array.prototype.first = function () { return (this.length === 0 ? null : this[0]); };
+Array.prototype.last = function () { return (this.length === 0 ? null : this[this.length - 1]); };
+Element.prototype.val = function (value) { return (typeof value === "undefined" ? this.value : this.value = value); };
 
 String.prototype.htmlEncode = function () {
     var map = { "&": "&amp;", "\"": "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;" };
